f9654277d7ad5e93ad0cf6073c555ec01478b345
jaysonmk2 <76066308+jaysonmk2@users.noreply.github.com>
2021-11-26 10:57:27 -0800

    user added

--- Home/Home/urls.py
+++ Home/Home/urls.py
@@ -16,14 +16,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
-
+from django.contrib.auth import views as auth_views
+from users import views as user_views
 
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('FormPage.urls'))
+    path('admin/', admin.site.urls, name='admin-page'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name="login"),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name="logout"),
+    path('', include('FormPage.urls'), name='home')
 ]
 
 if settings.DEBUG == True:
